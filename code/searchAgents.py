a15f1d64eed92ecd21df1becb3057db733676e10
egenereux <egenereux@orthogone.com>
2025-02-16 21:51:18 -0500

    restored q7 documentation

--- code/searchAgents.py
+++ code/searchAgents.py
@@ -505,11 +505,32 @@ def minimum_spanning_tree_cost(points):
 
 def foodHeuristic(state, problem):
     """
-    Heuristique avancée pour le FoodSearchProblem basée sur :
-    - La distance de Manhattan au point de nourriture le plus proche
-    - Le coût du MST reliant tous les points de nourriture
+    Your heuristic for the FoodSearchProblem goes here.
+    This heuristic must be consistent to ensure correctness.  First, try to come
+    up with an admissible heuristic; almost all admissible heuristics will be
+    consistent as well.
+    If using A* ever finds a solution that is worse uniform cost search finds,
+    your heuristic is *not* consistent, and probably not admissible!  On the
+    other hand, inadmissible or inconsistent heuristics may find optimal
+    solutions, so be careful.
+    The state is a tuple ( pacmanPosition, foodGrid ) where foodGrid is a Grid
+    (see game.py) of either True or False. You can call foodGrid.asList() to get
+    a list of food coordinates instead.
+    If you want access to info like walls, capsules, etc., you can query the
+    problem.  For example, problem.walls gives you a Grid of where the walls
+    are.
+    If you want to *store* information to be reused in other calls to the
+    heuristic, there is a dictionary called problem.heuristicInfo that you can
+    use. For example, if you only want to count the walls once and store that
+    value, try: problem.heuristicInfo['wallCount'] = problem.walls.count()
+    Subsequent calls to this heuristic can access
+    problem.heuristicInfo['wallCount']
     """
     current_position, foodGrid = state
+    '''
+        INSÉREZ VOTRE SOLUTION À LA QUESTION 7 ICI
+    '''
+
     food_positions = foodGrid.asList()
     
     if not food_positions:
